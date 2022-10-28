a158b5152cc5673803cd32279674a3d316b35773
Ryan York <ryanayork@gmail.com>
2022-10-28 10:15:50 -0700

    added darken color function

--- R/arcadia.pal.R
+++ R/arcadia.pal.R
@@ -63,3 +63,21 @@ arcadia.pal = function(n = 3,
     return(pal[1:n])
   }
 }
+
+
+
+#' Darken colors by a desired amount
+#'
+#' @param color Color(s) to darken.
+#' @param factor Factor to darken by.
+#' @export
+#' @examples
+#' darken_color('blue', factor = 1.5)
+#' darken_color(arcadia.pal(n = 3, name = 'Neutral'))
+
+darken_color <- function(color, factor=1.4){
+  col <- col2rgb(color)
+  col <- col/factor
+  col <- rgb(t(col), maxColorValue=255)
+  col
+}
